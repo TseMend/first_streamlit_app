c181121802510278e11cf027b52e17e7cedf4f0e
TseMend <104656931+TseMend@users.noreply.github.com>
2023-09-12 10:36:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ streamlit.header("FRUITLIST CONTAINS:")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     #my_cur.execute("use role accountadmin")
-    my_cur.execute("use role accountadmin select * from fruit_load_list")
+    my_cur.execute("use role accountadmin; select * from fruit_load_list")
     return my_cur.fetchall()
 
 if streamlit.button('Get Fruit Load List'):
